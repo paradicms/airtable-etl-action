35aa4a3262588379dc76d7f0880edc5e4ee5b732
Minor Gordon <minorg@server.fake>
2023-04-25 16:29:03 -0400

    adapt ETL actions to paradicms

--- action.py
+++ action.py
@@ -2,6 +2,7 @@
 import dataclasses
 from dataclasses import dataclass
 
+from etl_github_action import EtlGitHubAction
 from paradicms_etl.extractors.costume_core_data_airtable_extractor import (
     CostumeCoreDataAirtableExtractor,
 )
@@ -9,58 +10,57 @@ from paradicms_etl.pipeline import Pipeline
 from paradicms_etl.transformers.costume_core_data_airtable_transformer import (
     CostumeCoreDataAirtableTransformer,
 )
-from paradicms_ssg.github_action import GitHubAction
-from paradicms_ssg.github_action_inputs import GitHubActionInputs
 
 
-@dataclass(frozen=True)
-class _Inputs(GitHubActionInputs):
-    airtable_access_token: str = dataclasses.field(
-        default=GitHubActionInputs.REQUIRED,
-        metadata={
-            "description": "Airtable access token (not API key), see https://support.airtable.com/docs/creating-and-using-api-keys-and-access-tokens"
-        },
-    )
-    airtable_base_id: str = dataclasses.field(
-        default=GitHubActionInputs.REQUIRED,
-        metadata={
-            "description": "Airtable base id such as appgU92SdGTwPIVNg, see the base API documentation"
-        },
-    )
-    profile: str = dataclasses.field(
-        default="costume_core",
-        metadata={
-            "description": "Application profile of the data/metadata in the Airtable metadata, one of: costume_core"
-        },
-    )
-
-
-class Action(GitHubAction[_Inputs]):
+class Action(EtlGitHubAction):
     """
-    Generate a static site from a Paradicms-formatted Airtable base.
+    Extract, transform, and load data from a Paradicms-formatted Airtable base.
     """
 
-    @classmethod
-    @property
-    def _inputs_class(cls):
-        return _Inputs
+    @dataclass(frozen=True)
+    class Inputs(EtlGitHubAction.Inputs):
+        airtable_access_token: str = dataclasses.field(
+            default=EtlGitHubAction.Inputs.REQUIRED,
+            metadata={
+                "description": "Airtable access token (not API key), see https://support.airtable.com/docs/creating-and-using-api-keys-and-access-tokens"
+            },
+        )
+        airtable_base_id: str = dataclasses.field(
+            default=EtlGitHubAction.Inputs.REQUIRED,
+            metadata={
+                "description": "Airtable base id such as appgU92SdGTwPIVNg, see the base API documentation"
+            },
+        )
+        profile: str = dataclasses.field(
+            default="costume_core",
+            metadata={
+                "description": "Application profile of the data/metadata in the Airtable metadata, one of: costume_core"
+            },
+        )
+
+    def __init__(
+        self, *, airtable_access_token: str, airtable_base_id: str, profile: str, **kwds
+    ):
+        EtlGitHubAction.__init__(self, **kwds)
+        self.__airtable_access_token = airtable_access_token
+        self.__airtable_base_id = airtable_base_id
+        self.__profile = profile.lower()
 
     def _run(self):
-        profile = self._inputs.profile.lower()
-        if profile == "costume_core":
+        if self.__profile == "costume_core":
             extractor = CostumeCoreDataAirtableExtractor(
-                access_token=self._inputs.airtable_access_token,
-                base_id=self._inputs.airtable_base_id,
+                access_token=self.__airtable_access_token,
+                base_id=self.__airtable_base_id,
                 extracted_data_dir_path=self._extracted_data_dir_path,
             )
             transformer = CostumeCoreDataAirtableTransformer()
         else:
-            raise NotImplementedError(f"profile: {profile}")
+            raise NotImplementedError(f"profile: {self.__profile}")
 
         Pipeline(
             extractor=extractor,
-            id=self._inputs.pipeline_id,
-            loader=self._create_loader(),
+            id=self._pipeline_id,
+            loader=self._loader,
             transformer=transformer,
         ).extract_transform_load(force_extract=self._force_extract)
 
